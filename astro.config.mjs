a8fa87be74a31feaf0e802f4f8ab064db802950c
hudagraph <87654787+hudagraph@users.noreply.github.com>
2025-11-27 08:46:24 +0700

    Add sitemap integration and Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap'; // Ini otomatis ada stlh install
 
-// https://astro.build/config
 export default defineConfig({
+  // PENTING: Ganti dengan domain asli kamu (harus https)
+  site: 'https://jejakaksi.com', 
+  
   integrations: [
     react(), 
-    tailwind({
-      // Menerapkan base styles secara otomatis
-      applyBaseStyles: false, 
-    })
+    tailwind({ applyBaseStyles: false }),
+    sitemap() // Ini otomatis ada stlh install
   ],
 });
